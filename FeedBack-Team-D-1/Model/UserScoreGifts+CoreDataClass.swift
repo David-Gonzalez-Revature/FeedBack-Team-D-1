32581502812e00fd06a041ecb75b3e23beb12817
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-05 08:20:30 -0500

    Add new CoreData and move this to Model File 2

---
+++ FeedBack-Team-D-1/Model/UserScoreGifts+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  UserScoreGifts+CoreDataClass.swift
+//  FeedBack-Team-D-1
+//
+//  Created by David Gonzalez on 3/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserScoreGifts)
+public class UserScoreGifts: NSManagedObject {
+
+}
+ 
---
+++ UserScoreGifts+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserScoreGifts+CoreDataClass.swift
+//  FeedBack-Team-D-1
+//
+//  Created by David Gonzalez on 3/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserScoreGifts)
+public class UserScoreGifts: NSManagedObject {
+
+}
