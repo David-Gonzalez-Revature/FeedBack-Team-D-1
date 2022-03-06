e6c5d4b6b2eb6b0478d8acaeede4bc30c6972837
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-06 18:33:09 -0500

    Add new-features ranking page

--- FeedBack-Team-D-1/ViewControl/Controller/GiftUITableViewController.swift
+++ FeedBack-Team-D-1/ViewControl/Controller/GiftUITableViewController.swift
@@ -20,14 +20,24 @@ class GiftUITableViewController: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        print("GiftUITableViewController      aca:   awakeFromNib")
         // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
+        if selected{
+            print("1 GiftUITableViewController  setSelected",selected.self.description)
+        }else{
+            print("Not Selected",selected)
+        }
+        
+       
         // Configure the view for the selected state
     }
+    
+  
     
     
 }
