2a564c86b7baf8e86258caccb0634cb5d672fb1b
john726 <revature726@revature.net>
2022-03-08 22:01:07 -0800

    updated splash animation

---
+++ FeedBack-Team-D-1/ViewControl/Controller/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  FeedBack-Team-D-1
+//
+//  Created by John Figueroa on 3/8/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FeedBack-Team-D-1/ViewControl/Controller/SplashscreenAnimationViewController.swift
+++ FeedBack-Team-D-1/ViewControl/Controller/SplashscreenAnimationViewController.swift
@@ -12,24 +12,59 @@ class SplashscreenAnimationViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
     func animateRotation(){
-        UIView.animate(withDuration: 2.75, animations: {
+        UIView.animate(withDuration: 1.75, animations: {
             self.image.transform = CGAffineTransform(rotationAngle:  .pi)
         })
-        UIView.animate(withDuration: 3.0, animations: {
+        UIView.animate(withDuration: 2.0, animations: {
             self.image.transform = CGAffineTransform(rotationAngle:  2 * .pi)
-        })
+                    })
     }
-    func startAnimate(){
-        print("function was called")
-        
-        var timer = Timer.scheduledTimer(timeInterval: 2.75, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true) // target where it shoudl find selector self means current one
+    func endAnimation(){
+        UIView.animate(withDuration: 1.75, animations: {
+            self.image.transform = CGAffineTransform(rotationAngle:  .pi)
+        })
+        UIView.animate(withDuration: 2.0, animations: {
+            self.image.transform = CGAffineTransform(rotationAngle:  2 * .pi)
+            self.image.alpha = 0
+                    })
     }
     
-    @objc func updateImage(){//argument of #selector refers to instance method updateImage() that is not exposed to o-c; add @objc to expose instance method to o-c
-        
-        var count = 5
-        repeat{
+    
+    func startAnimate(){
+        print("function was called")
+        var timerCount = 0
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.75, repeats: true) { timer in
+            timerCount += 1
             
+            print(timerCount)
+            if(timerCount == 3){
+                self.endAnimation()
+                timer.invalidate()
+                let navigateTime = Timer.scheduledTimer(withTimeInterval: 3.75, repeats: false){
+                    navigateTime in self.navigateToController()
+                }
+            }
+            var count = 3
+            repeat{
+                self.animateRotation()
+                count -= 1
+            } while(count > 0)
+        }
+        
+        //Timer.scheduledTimer(timeInterval: 1.75, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true) // target where it shoudl find selector self means current one
+    }
+    
+    //var functionCall = 0
+    @objc func updateImage(){//argument of #selector refers to instance method updateImage() that is not exposed to o-c; add @objc to expose instance method to o-c
+        /*
+        functionCall += 1
+        print(functionCall)
+        if(functionCall == 3){
+            endAnimation()
+            //repeats = false
+        }*/
+        var count = 3
+        repeat{
             animateRotation()
             count -= 1
         } while(count > 0)
@@ -105,7 +140,7 @@ class SplashscreenAnimationViewController: UIViewController {
     
     func navigateToController(){
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "startView") as! StartViewController
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "startView") as! HomeViewController
         self.present(nextViewController, animated:true, completion:nil)
     }
     
