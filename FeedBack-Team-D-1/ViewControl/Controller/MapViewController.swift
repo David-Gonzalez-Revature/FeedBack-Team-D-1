563a31bf897e6f98eda92e71732acb32dbf63b0b
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-10 07:05:08 -0500

    add Map in Gift

---
+++ FeedBack-Team-D-1/ViewControl/Controller/MapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MapViewController.swift
+//  FeedBack-Team-D-1
+//
+//  Created by David Gonzalez on 3/10/22.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var myMap: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      //  17.2220° N, 89.6237° W
+        
+            let lc = CLLocationCoordinate2D(latitude: 17.2220, longitude: -89.6237)
+            
+            let ms = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+            
+            let rg = MKCoordinateRegion(center: lc, span: ms)
+            
+            //kaplm
+        myMap.setRegion(rg,  animated: true)
+                           
+            let annot = MKPointAnnotation()
+                           
+            annot.coordinate = lc
+             annot.title = "City Escape Provider"
+            annot.subtitle = "Change Coupon Here"
+        myMap.addAnnotation(annot)
+    }
+    
+
+    // MARK: - Navigation
+}
